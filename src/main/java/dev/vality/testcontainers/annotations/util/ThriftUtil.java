0246ec3cdd6dad7926490893eeebb6585309ed67
Anatoly Karlov <karleowne@gmail.com>
2022-02-01 19:38:00 +0700

    TD-130: remove business protos, remove rbk, bump deps

--- src/main/java/dev/vality/testcontainers/annotations/util/ThriftUtil.java
+++ src/main/java/dev/vality/testcontainers/annotations/util/ThriftUtil.java
@@ -5,11 +5,8 @@ import dev.vality.geck.serializer.kit.json.JsonHandler;
 import dev.vality.geck.serializer.kit.json.JsonProcessor;
 import dev.vality.geck.serializer.kit.tbase.TBaseHandler;
 import dev.vality.geck.serializer.kit.tbase.TBaseProcessor;
-import dev.vality.machinegun.msgpack.Value;
 import lombok.SneakyThrows;
 import org.apache.thrift.TBase;
-import org.apache.thrift.TSerializer;
-import org.apache.thrift.protocol.TBinaryProtocol;
 
 public class ThriftUtil {
 
@@ -21,12 +18,5 @@ public class ThriftUtil {
     @SneakyThrows
     public static <T extends TBase> T jsonToThrift(JsonNode jsonNode, Class<T> type) {
         return new JsonProcessor().process(jsonNode, new TBaseHandler<>(type));
-    }
-
-    @SneakyThrows
-    public static Value toByteArray(TBase<?, ?> thrift) {
-        return Value.bin(
-                new TSerializer(new TBinaryProtocol.Factory())
-                        .serialize(thrift));
     }
 }
